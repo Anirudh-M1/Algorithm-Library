70c9a64543a9768b92087d0dfff2695af25cb593
Anirudh Moholkar <143670965+Anirudh-M1@users.noreply.github.com>
2026-06-06 14:03:38 -0500

    Add: surrounded-regions - submission-5

---
+++ Data Structures & Algorithms/surrounded-regions/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:      
+        visited = set()
+        free = set()
+        neighbors = [(1,0), (0,1), (-1, 0), (0, -1)]
+        def dfs(i,j): 
+            for x,y in neighbors:
+                nx, ny = i + x, j + y
+                print("checking Neighbors")
+                if 0<=nx<len(board) and 0<ny<len(board[0]) and (nx,ny) not in visited and board[nx][ny] == "O":
+                    print("added to free")
+                    free.add((nx,ny))
+                    visited.add((nx,ny))
+                    dfs(nx,ny)
+
+        for i in range(len(board)):
+            for j in range(len(board[0])): 
+                if board[i][j] == "O" and (i == 0 or i == len(board) - 1 or j == 0 or j == len(board[0])-1):
+                    free.add((i,j))
+                    print(free)
+                    dfs(i,j)
+
+        for i in range(len(board)):
+            for j in range(len(board[0])): 
+                if (i,j) not in free:
+                    board[i][j] = "X"
+        print(board)
