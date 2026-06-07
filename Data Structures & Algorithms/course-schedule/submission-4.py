b6f69ceb909a4a8a8dfb9b24273d9220b2d86625
Anirudh Moholkar <143670965+Anirudh-M1@users.noreply.github.com>
2026-06-07 11:59:27 -0500

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = defaultdict(list)
+
+        for p in prerequisites:
+            preMap[p[0]].append([p[1]])
+
+        visited = set()
+        self.canfin = True
+
+
+        def dfs(i): 
+            if preMap[i] == []: 
+                return True
+            
+            if i in visited:
+                self.canfin = False
+                return False
+            
+            visited.add(i)
+
+            for j in preMap[i]: 
+                dfs(j[0]) 
+                
+            preMap[i] = []
+        
+            visited.remove(i)
+
+
+        for p in range(numCourses):
+            dfs(p)
+
+        return self.canfin
