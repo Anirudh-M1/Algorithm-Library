0b0a68c473092e7cc5bbe72c60051e314e9f1a68
Anirudh Moholkar <143670965+Anirudh-M1@users.noreply.github.com>
2026-06-08 23:03:52 -0500

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        self.count = 0
+        
+        arr = [0] * (n+1)
+
+        def dfs(num):
+            if arr[num]: 
+                return arr[num]
+
+            if num < 0:
+                return 0
+            if num == 0:
+                return 1
+
+            arr[num] =  dfs(num - 1) + dfs(num - 2)
+
+            return arr[num]
+           
+        return dfs(n)
