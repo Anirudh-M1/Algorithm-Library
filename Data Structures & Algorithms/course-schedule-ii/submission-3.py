3cd2e8a234b67592a9a82bc026ad6cb5d10370b0
Anirudh Moholkar <143670965+Anirudh-M1@users.noreply.github.com>
2026-06-10 00:36:51 -0500

    Add: course-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = [[] for i in range(numCourses)]        
+        for d,s in prerequisites: 
+            adj[d].append(s)
+
+        ans = []
+        visit = [0]*numCourses
+        def dfs(courses): 
+            nonlocal ans
+            if visit[courses] == 1: 
+                return False
+            if visit[courses] == 2: 
+                return True
+
+            visit[courses] = 1 
+            
+            for nxt_course in adj[courses]: 
+                if not dfs(nxt_course): 
+                    return False
+
+            visit[courses] = 2
+            ans.append(courses)
+            return True
+
+        for i in range(numCourses): 
+            if not dfs(i): 
+                return []
+        return ans
