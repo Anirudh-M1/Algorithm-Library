4ef4fd6cb28e880fd73b1abd73a80137a2fe2dd7
Anirudh Moholkar <143670965+Anirudh-M1@users.noreply.github.com>
2026-06-13 17:20:42 -0500

    Add: min-cost-climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        #Top down cache 
+        minCost = float("inf")
+        
+        minCosts =  {}
+        def dfs(n, currentCost): 
+            if n in minCosts:
+                return minCosts[n]
+            if n >= len(cost):
+                return currentCost
+
+            minCosts[n] = min(dfs(n +1, currentCost), dfs(n+2, currentCost)) + cost[n]
+            return minCosts[n]
+
+        return min(dfs(1, 0), dfs(0, 0))
