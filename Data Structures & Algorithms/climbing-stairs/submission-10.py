ced3a09a0ca094282008c98380acafed9b373470
Anirudh Moholkar <143670965+Anirudh-M1@users.noreply.github.com>
2026-06-13 16:59:22 -0500

    Add: climbing-stairs - submission-10

---
+++ Data Structures & Algorithms/climbing-stairs/submission-10.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+     
+        seen = {}
+        seen[0] = 1
+        seen[1] = 1
+        seen[2] = 2
+        for i in range(2,n +1): 
+            seen[i] = seen[i-1] + seen[i-2]
+        
+        return seen[n]
+       
