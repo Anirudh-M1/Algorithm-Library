42295d13318bbe61e4ce7a589d1785e7ca96334e
Anirudh Moholkar <143670965+Anirudh-M1@users.noreply.github.com>
2026-06-18 23:20:39 -0500

    Add: min-cost-to-connect-points - submission-6

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-6.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        
+        minDist = [float("inf")] * len(points)
+        visited = set()
+
+        minDist[0] = 0
+        cost = 0
+
+        while len(visited) != len(points): 
+            curDist = float("inf")
+            nextClosest = -1
+            for i in range(len(points)): 
+                if i not in visited and minDist[i] < curDist: 
+                    curDist = minDist[i]
+                    nextClosest = i
+            
+            x1, y1 = points[nextClosest]
+            cost += curDist
+            visited.add(nextClosest)
+
+            for p in range(len(points)): 
+                if p not in visited:
+                    x2, y2 = points[p]
+                    dist = abs(x1 - x2) + abs(y1 - y2)
+                    if dist < minDist[p]: 
+                        minDist[p] = dist
+            
+        return cost
