2e2db9f5b5b8042817b183a8a1d63885232f11fe
Anirudh Moholkar <143670965+Anirudh-M1@users.noreply.github.com>
2026-07-01 23:35:21 -0500

    Add: implement-prefix-tree - submission-6

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-6.py
@@ -0,0 +1,39 @@
+class Node: 
+    def __init__(self):
+        self.chars = {}
+
+class PrefixTree:
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for c in word:
+            if c not in node.chars:
+                node.chars[c] = Node()
+            
+            node = node.chars[c]
+        
+        node.chars["end"] = True
+        
+    def search(self, word: str) -> bool:
+        node = self.root
+        for c in word:
+            if c in node.chars:
+                node = node.chars[c]
+            else:
+                return False
+        
+        if "end" in node.chars: 
+            return True
+        else:
+            return False
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for c in prefix:
+            if c in node.chars:
+                node = node.chars[c]
+            else:
+                return False
+        return True
